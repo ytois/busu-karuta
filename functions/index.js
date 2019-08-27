7617c48a0f07b83f03a21054b23993cf58d9b544
oishi-yuta <oishi_yuta@microad.co.jp>
2019-08-27 14:56:51 +0900

    create functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,57 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const express = require('express')
+const app = express()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// 現在のゲームを取得. 無い場合は新たに作る
+app.get('/api/game', (request, response) => {
+  const json = {
+    id: 'xxxxxxxxx',
+    status: 'active',
+    created_at: '2019-08-01 12:00:00',
+    remainingCard: 50,
+  }
+  response
+    .status(200)
+    .json(json)
+    .end()
+})
+
+// 回答する
+app.post('/api/game/:gameId/answer', (request, response) => {
+  const json = {
+    collect: true,
+    next_card: {
+      id: 1,
+      text: 'hoge',
+    },
+  }
+  response
+    .status(200)
+    .json(json)
+    .end()
+})
+
+// ゲームを中断する
+app.post('/api/game/:gameId/revoke', (request, response) => {
+  const json = {
+    status: 'ok',
+  }
+  response
+    .status(200)
+    .json(json)
+    .end()
+})
+
+app.get('/api/ranking', (request, response) => {
+  const ranking = [
+    { userName: 'user1', score: 100, rank: 1 },
+    { userName: 'user2', score: 90, rank: 2 },
+    { userName: 'user3', score: 80, rank: 3 },
+  ]
+  response
+    .status(200)
+    .json(ranking)
+    .end()
+})
+
+exports.app = functions.https.onRequest(app)
